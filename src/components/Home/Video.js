5938ddc320a29ed34f888e0bf2f3036300db5a9e
Guido Greco <guidogreco@Guidos-MacBook-Pro.local>
2021-11-03 14:22:51 -0300

    video correction

--- src/components/Home/Video.js
+++ src/components/Home/Video.js
@@ -1,8 +1,8 @@
-import pruebavideo from '../../assets/Video/Fondo/pruebavideo.mp4';
+
 import styles from './Video.module.css'
 
 const Video = () => {
-    console.log(pruebavideo)
+    const pruebavideo = 'https://res.cloudinary.com/malks14/video/upload/v1635960035/Suca/pruebavideo_volpac.mp4'
     return (
         <video className={styles.video} autoPlay loop muted id='video'>
             <source src={pruebavideo} type='video/mp4'/>
